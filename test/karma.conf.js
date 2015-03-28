d4d92caf3a321025b95d341399f701d105c03632
MatMath <mathieu.k.legault@gmail.com>
2015-03-28 15:22:09 +0700

    Integration of the test folder from PhoneGap

---
+++ test/karma.conf.js
@@ -0,0 +1,32 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : '../',
+
+    files : [
+      // 'www/lib/ionic/js/angular/angular.js',
+      'www/lib/ionic/js/ionic.bundle.js',
+      // 'www/lib/ionic/js/bower_components/angular-mocks/angular-mocks.js',
+      // 'www/lib/ionic/js/**/*.js',
+      'test/unit/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
